5b9f74cf7dad6ccde5992963e4e7f9f63cb8d352
Habtamu Asegahegn <34951324+HabtamuDes@users.noreply.github.com>
2023-05-10 01:57:04 -0700

    monog_db_connect

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello node api")
-})
-app.listen(3000, ()=> {
-    console.log(`Connection is running on port 3000`)
-})
+mongoose
+  .connect("mongodb://127.0.0.1:27017/test")
+  .then(() => {
+    console.log("Mongo Db Connected!");
+
+    app.listen(3000, () => {
+      console.log(`Connection is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.get("/", (req, res) => {
+  res.send("Hello node api");
+});
+
+app.get("/blog", (req, res) => {
+  res.send("Hello node blog");
+});
